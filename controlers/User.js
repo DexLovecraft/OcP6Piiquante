5e5ef5b77e244a974d4313221ac18d7c4575decb
Alex Balmes <alex.balmes1912@gmail.com>
2023-02-22 15:03:19 +0100

    creation of auth token and auth token psuh in req

--- controlers/User.js
+++ controlers/User.js
@@ -1,5 +1,6 @@
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -21,20 +22,24 @@ exports.login = (req, res, next) => {
         .then(user => {
             if(!user){
                 return res.status(401).json({ message : "paire Email/Mot de passe invalide"})
-            }
+            };
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if(!valid){
                         return res.status(401).json({ message : "paire Email/Mot de passe invalide"})
-                    }
+                    };
                     res.status(200).json({
                         userId : user._id,
-                        token : 'TOKEN'
-                    })
+                        token: jwt.sign(
+                            { userId: user._id },
+                            '$2a$12$dCCL6qWZNv1Zr5UpY4NGNOLlAcTcAalOQyU0YB9K35J5fwHhwnt',
+                            { expiresIn: '24h' }
+                        )
+ 
+                    });
                 })
                 .catch(error => res.status(500).json({error}));
         })
         .catch(error => res.status(500).json({error}));
-      }, 250)
-    
+      }, 500);
 };
